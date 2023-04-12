c39343b4e96c9953c0379e17f0ca23dc1b58b20f
Theresa Gasser <tgasser@udallas.edu>
2023-04-12 18:51:18 -0500

    completed getClassDay() helper

--- functions/index.js
+++ functions/index.js
@@ -21,17 +21,12 @@ import * as helpers from "./database-helper-funcs/base-requests.js";
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
 
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 //   exports.randomNumber = functions.https.onRequest((request, response) => {
 //   const number = Math.round(Math.random() * 100);
 //   response.send(number.toString());
 //   })
   
-let x = helpers.get(helpers.base.urlstudents())
+
 // exports.checkInStudent = functions.https.onRequest((request, response) => {
 // 	// Get from the request body the student ID and Class ID
  
@@ -46,18 +41,15 @@ let x = helpers.get(helpers.base.urlstudents())
 // 	// Update classID in firebase
 // 	functions.update.classDay = thisClass;
 
-// 	functions.logger.info("Hello logs!", {structuredData: true});
-//   	response.send("Hello from Firebase!");
-// })
 
-// exports.randomNumber = functions.https.onRequest((request, response) => {
-// const number = Math.round(Math.random() * 100);
-// response.send(number.toString());
-// })
 
 // // callable function example
 // // I think for many of our functions this is the type we want to be using because it allows for client-side to call them
 // //https://www.youtube.com/watch?v=8mL1VuiL5Kk&list=PL4cUxeGkcC9i_aLkr62adUTJi53y7OjOf&index=5
-// exports.sayHello = functions.https.onCall((data, context) =>{
-// return 'hello mid team'
-// })
+
+function getClassDay(key){
+	let list_of_classdays = helpers.get(helpers.base.urlclass_days()); 
+	for(const i in list_of_classdays){
+		console.log(JSON.stringify[i]);
+	}
+}
