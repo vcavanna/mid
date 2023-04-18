de2cb170d302e423b5858408a0e62c2e7eeb4a63
Pat <pkearney@udallas.edu>
2023-04-18 14:36:31 -0500

    "class checkin function should be working with sample function"

--- functions/index.js
+++ functions/index.js
@@ -19,62 +19,58 @@ const functions = require("firebase-functions");
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
-
-  exports.randomNumber = functions.https.onRequest((request, response) => {
-  const number = Math.round(Math.random() * 100);
-  response.send(number.toString());
-  })
-  
 
 exports.checkInStudent = functions.https.onRequest((request, response) => {
 	// Get from the request body the student ID and Class ID
-  // url would be something like https.{firebase_functions_url}.com/example?sid=900887873&cid=1234
-  const studentID = request.query.sid;
-  const classID = request.query.cid;
+	// url would be something like https.{firebase_functions_url}.com/example?sid=900887873&cid=1234
+	const studentID = request.query.sid;
+	const classID = request.query.cid;
 
- 
+
 	// Generate a key for the "class day":
-  
+
 	var date = getDateTime(); //Patrick. Returns dat in MMDDYYYY format
 	var key = genClassDayKey(student.ID, date); // Veep
 
 	// Add studentID to list in class day struct:
 	//const today = apiHelpers.apiGetRequest
-  	
+
 	//this should add a student to the attendance object
-	thisclassDate.attendance = {studentID, date} // Seb
-	
+	thisclassDate.attendance = { studentID, date } // Seb
+
 	// Update classID in firebase
 	functions.update.classDay = thisClass;
 
-	functions.logger.info("Hello logs!", {structuredData: true});
-  	response.send("student Checked In... Probably");
+	functions.logger.info("Hello logs!", { structuredData: true });
+	response.send("student Checked In... Probably");
 })
 
-exports.randomNumber = functions.https.onRequest((request, response) => {
-const number = Math.round(Math.random() * 100);
-response.send(number.toString());
-})
+
 
 // callable function example
 // I think for many of our functions this is the type we want to be using because it allows for client-side to call them
 //https://www.youtube.com/watch?v=8mL1VuiL5Kk&list=PL4cUxeGkcC9i_aLkr62adUTJi53y7OjOf&index=5
-exports.sayHello = functions.https.onCall((data, context) =>{
-return 'hello mid team'
+
+
+exports.classCheckin = functions.https.onRequest((request, response) => {
+	const text = {"aB2f":{"classDates":[4112023],"name":"Intro","students":[900887583]}};
+	const classID = getDateTime() + "_" + getClassKey(text);
+	//document.write(classID);
+	response.send(classID);
 })
 
-
 function genClassDayKey(studentID, date) {
-	return studentID + '_' + date;
+	return studentID + '_' + getDateTime();
+}
+// helper function to turn JSON into a string array
+function getClassKey(text, obj) {
+	const newObj = Object.keys(text)[0];
+	return newObj;
 }
 
-function getDateTime(){
+function getDateTime() {
 
 	var date = new Date();
 	return date.toISOString();
-	}
+}
 
