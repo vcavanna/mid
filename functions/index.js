4784e3d850ea6127c639ab2cc85624804ab33611
Theresa Gasser <tgasser@udallas.edu>
2023-04-18 14:54:03 -0500

    updated my index.js

--- functions/index.js
+++ functions/index.js
@@ -4,36 +4,52 @@
 const functions = require("firebase-functions");
 const helpers = require("./database-helper-funcs/base-requests.js");
 
-//see ISO-8601 for date time use this for 
+// deleting a function from deployment:
+// firebase functions:delete myFunction
 
-exports.randomNumber = functions.https.onRequest((request, response) => {
-const number = Math.round(Math.random() * 100);
-response.send(number.toString());
-})
+// Playing around with the "onCreate" function: this example replaces any student's name that's added to the database with "Theresa"
+// so: if you deploy this function, then add a student to the database via postman, the student's name will be changed
+// the "onCreate" function may be very useful for other things we want implemented in our database
+
+// exports.onStudentAdd= functions.database
+// .ref('/students/{studentID}')
+// .onCreate((snapshot, context) => {
+// 	const curStu = context.params.studentID
+// 	const studentVals = snapshot.val() 
+
+// 	return snapshot.ref.update({StudentName: 'Theresa'})
+// })
+
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
 
 
 exports.checkInStudent = functions.https.onRequest((request, response) => {
 	// Get from the request body the student ID and Class ID
-  // url would be something like https.{firebase_functions_url}.com/example?sid=900887873&cid=1234
-  const studentID = request.query.sid;
-  const classID = request.query.cid;
+	// url would be something like https.{firebase_functions_url}.com/example?sid=900887873&cid=1234
+	const studentID = request.query.sid;
+	const classID = request.query.cid;
+
 
 	// Generate a key for the "class day":
 
-	const date = getToday(); //Pat wrote this but it is in the ether now
-	const key = genClassDayKey(studentID, date);
+	var date = getDateTime(); //Patrick. Returns dat in MMDDYYYY format
+	var key = genClassDayKey(student.ID, date); // Veep
 
 	// Add studentID to list in class day struct:
-	const today = apiHelpers.apiGetRequest
-  
-	thisclassDate.attendance = {studentID, date} // Seb
-	
+	//const today = apiHelpers.apiGetRequest
+
+	//this should add a student to the attendance object
+	thisclassDate.attendance = { studentID, date } // Seb
+
 	// Update classID in firebase
 	functions.update.classDay = thisClass;
 
-	functions.logger.info("Hello logs!", {structuredData: true});
-  	response.send("student Checked In... Probably");
+	functions.logger.info("Hello logs!", { structuredData: true });
+	response.send("student Checked In... Probably");
 })
+
 
 
 // callable function example
@@ -41,23 +57,24 @@ exports.checkInStudent = functions.https.onRequest((request, response) => {
 //https://www.youtube.com/watch?v=8mL1VuiL5Kk&list=PL4cUxeGkcC9i_aLkr62adUTJi53y7OjOf&index=5
 
 
+exports.classCheckin = functions.https.onRequest((request, response) => {
+	const text = {"aB2f":{"classDates":[4112023],"name":"Intro","students":[900887583]}};
+	const classID = getDateTime() + "_" + getClassKey(text);
+	//document.write(classID);
+	response.send(classID);
+})
+
 function genClassDayKey(studentID, date) {
-	return studentID + '_' + date;
-
-
+	return studentID + '_' + getDateTime();
 }
-function getToday(){
-	var today = new Date(); //use function today.toISOString()
-	var dd = String(today.getDate()).padStart(2, '0');
-	var mm = String(today.getMonth() + 1).padStart(2, '0');
-	var yyyy = today.getFullYear();
-	
-	today = mm + dd + yyyy;
-	return today;
+// helper function to turn JSON into a string array
+function getClassKey(text, obj) {
+	const newObj = Object.keys(text)[0];
+	return newObj;
 }
 
-async function getClassDay(key){ // is given a class key: example usage: //getClassDay("04112023_aB2f");
-	let key_JSON= await helpers.get(helpers.base().append(helpers.base().class_days, key)); // gets the node associated with classdays
-	console.log(key_JSON);
-	return key_JSON;
+function getDateTime() {
+
+	var date = new Date();
+	return date.toISOString();
 }
