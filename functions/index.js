e5cab74ed17db8811839395ea52aded66102ca64
Theresa Gasser <tgasser@udallas.edu>
2023-04-30 11:04:23 -0500

    fixed issue with put, added registerStudent

--- functions/index.js
+++ functions/index.js
@@ -4,64 +4,61 @@
 const functions = require("firebase-functions");
 const helpers = require("database-helper-funcs");
 
+const baseURL = helpers.base()
+
 // deleting a function from deployment:
 // firebase functions:delete myFunction
-
-// Playing around with the "onCreate" function: this example replaces any student's name that's added to the database with "Theresa"
-// so: if you deploy this function, then add a student to the database via postman, the student's name will be changed
-// the "onCreate" function may be very useful for other things we want implemented in our database
-
-// exports.onStudentAdd= functions.database
-// .ref('/students/{studentID}')
-// .onCreate((snapshot, context) => {
-// 	const curStu = context.params.studentID
-// 	const studentVals = snapshot.val() 
-
-// 	return snapshot.ref.update({StudentName: 'Theresa'})
-// })
-
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
 
 exports.checkInStudent = functions.https.onRequest((request, response) => {
 	// Get from the request body the student ID and Class ID
 	// url would be something like https.{firebase_functions_url}.com/example?sid=900887873&cid=1234
 	const studentID = request.query.sid;
 	const classID = request.query.cid;
+	var date = getDateTime().slice(0, 10);
 
-
-	// Generate a key for the "class day":
-
-	var date = getDateTime(); //Patrick. Returns dat in MMDDYYYY format
-	var key = genClassDayKey(student.ID, date); // Veep
-
-	// Add studentID to list in class day struct:
-	//const today = apiHelpers.apiGetRequest
-
-	//this should add a student to the attendance object
-	thisclassDate.attendance = { studentID, date } // Seb
-
-	// Update classID in firebase
-	functions.update.classDay = thisClass;
-
-	functions.logger.info("Hello logs!", { structuredData: true });
-	response.send("student Checked In... Probably");
+	const classDayID = date + "_" + classID;
+	helpers.put(baseURL.append(baseURL.class_days, classDayID + "/attendance"), { studentID: getDateTime() });
+	response.send("student " + studentID + " Checked In at " + getDateTime() + " Probably");
 })
 
+async function checkInStudentTest() {
+	const studentID = 900888157;
+	const classID = "aB2f";
+	var date = getDateTime().slice(0, 10);
+
+	const classDayID = date + "_" + classID;
+	let recieved = await helpers.get(baseURL.append(baseURL.class_days, classDayID + "/attendance"))
+}
 
 
-// callable function example
-// I think for many of our functions this is the type we want to be using because it allows for client-side to call them
-//https://www.youtube.com/watch?v=8mL1VuiL5Kk&list=PL4cUxeGkcC9i_aLkr62adUTJi53y7OjOf&index=5
+async function classCheckinTest() {
 
+	const text = "6a407";
+	const IDs = await helpers.get(baseURL.append(baseURL.professors, text));
+	//console.log(IDs);
+	const classInfo = Object.values(IDs.classIDs);
+	//console.log(classInfo);
+	const classCheck = getDateTime().slice(0, 10) + "_" + classInfo;
+	helpers.put(baseURL.append(baseURL.class_days, classCheck), { "classDate": getDateTime(), "name": "test", "attendance": [" ",] });
 
-exports.classCheckin = functions.https.onRequest((request, response) => {
-	const text = {"aB2f":{"classDates":[4112023],"name":"Intro","students":[900887583]}};
-	const classID = getDateTime() + "_" + getClassKey(text);
+	const classDates = await helpers.get(baseURL.append(baseURL.classes, classInfo + "/classDates"));
+	classDates["Day" + ((Object.keys(classDates).length) + 1)] = getDateTime();
+	helpers.put(baseURL.append(baseURL.classes, classInfo + "/classDates"), classDates);
+}
+
+exports.classCheckin = functions.https.onRequest(async (request, response) => {
+	const text = request.query.profID;
+	const IDs = await helpers.get(baseURL.append(baseURL.professors, text));
+	const classInfo = IDs.classIDs[0];
+	const classCheck = getDateTime().slice(0, 10) + "_" + classInfo;
+	await helpers.put(baseURL.append(baseURL.class_days, classCheck), { "classDate": getDateTime, "name": "none", "attendance": [" ",] });
+
+	//putting the classDate into the classdates section in database
+	const classDates = await helpers.get(baseURL.append(baseURL.classes, classInfo + "/classDates"));
+	classDates["Day" + ((Object.keys(classDates).length) + 1)] = getDateTime();
+	await helpers.put(baseURL.append(baseURL.classes, classInfo + "/classDates"), classDates);
 	//document.write(classID);
-	response.send(classID);
+	response.status(200).send(classCheck);
 })
 
 function genClassDayKey(studentID, date) {
@@ -81,32 +78,37 @@ function getDateTime() {
 
 //example: https://us-central1-middleware-6a409.cloudfunctions.net/getProfessorAttendanceData?id=6a407
 exports.getProfessorAttendanceData = functions.https.onRequest(async (request, response) => { // given a prof id, output the info associated
+	//reformat data to be based on name:
+
 	let output = { "name": "",
 				   "classes": []
 	}
 	let class_obj = {"class_name": "",
-		"class_id": "", 
-		"class_days": [],
+					 "class_id": "", 
+  					 "class_days": [],
 	}
 	let profID = request.query.id;
 	if(profID == null){ // no id param given
 		response.status(404).send('Insufficient Parameters: \n No ID provided');
 	}
-	let baseURL = helpers.base(); 
 	let proNode = baseURL.professors + "/" + profID;
 	const profNode = await helpers.get(baseURL.append(proNode,""));
 	if(profNode == null){ // id is not found in the database
 		response.status(404).send('ID not found');
 	}
 	output.name = await profNode.ProfessorName; 
+
 	for(const i in await profNode.classIDs){
+
 		let classNode = await helpers.get(baseURL.append(baseURL.classes, profNode.classIDs[i]));
+
 		class_obj.class_name = await classNode.name; 
 		class_obj.class_id = profNode.classIDs[i];
+
 		let classDaysNode = await helpers.get(baseURL.urlclass_days());
 
 		for(const x in classDaysNode){
-			let cur = x.substring(x.length-4, x.length);
+			let cur = x.substring(x.length-4, x.length); //
 			if(cur == class_obj.class_id){
 				let class_day_id_obj = {
 					[x]: classDaysNode[x],
@@ -118,4 +120,26 @@ exports.getProfessorAttendanceData = functions.https.onRequest(async (request, r
 	}
 	response.send(output); 
 })
+
+//https://us-central1-middleware-6a409.cloudfunctions.net/registerStudent?sn=Theresa_Gasser&sid=23456
+exports.registerStudent = functions.https.onRequest(async (request, response) => { // puts a student into the database
+	let studentName =  request.query.sn;
+	let studentID = request.query.sid;
+	let obj = {"studentName": studentName}
+
+	if(studentName == null || studentID == null){
+		response.status(404).send('Insufficient parameters');
+	}
+	try {
+		await helpers.put(baseURL.append(baseURL.students, studentID),obj);
+		response.send(obj);
+	} catch(error) {
+		response.status(500).send(error);
+	}
+	response.status(500).send("this should not have happened");
+})
+
+
+
+
 
